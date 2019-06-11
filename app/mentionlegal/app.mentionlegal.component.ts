84aa71bf8467d0c3e9253da01d0c16b8459f920e
MaximeFernandes <maximefernandes42@gmail.com>
2019-06-11 10:18:21 +0200

    ajout composant mentionlegal

---
+++ app/mentionlegal/app.mentionlegal.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-mentionlegal',
+  templateUrl: 'app.mentionlegal.html'
+
+
+})
+export class MentionLegalComponent {
+ }
